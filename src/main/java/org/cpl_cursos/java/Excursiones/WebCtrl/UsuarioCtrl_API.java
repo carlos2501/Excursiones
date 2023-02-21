806882e344bae98e4b331f72313e3c742dd816b1
CPL <carlos@centropyme.com>
2023-02-21 01:16:38 +0100

    Completando API de usuarios...

--- src/main/java/org/cpl_cursos/java/Excursiones/WebCtrl/UsuarioCtrl_API.java
+++ src/main/java/org/cpl_cursos/java/Excursiones/WebCtrl/UsuarioCtrl_API.java
@@ -2,7 +2,6 @@ package org.cpl_cursos.java.Excursiones.WebCtrl;
 
 import org.cpl_cursos.java.Excursiones.modelos.Usuario;
 import org.cpl_cursos.java.Excursiones.servicios.UsuarioSrvcImpl;
-import org.hibernate.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,12 +33,46 @@ public class UsuarioCtrl_API {
         }
         return new ResponseEntity<>(listaUsu, HttpStatus.OK);
     }
+    // ----------------- Perfil de un usuario -------------------------
     @GetMapping("/{id}")
     public Optional<Usuario> buscaUsuarioPorId(@PathVariable Long id) {
         return this.srvc.buscarPorId(id);
     }
-
-    @PostMapping({"","/"})
-    public void guardarUsuario(){
+    // ----------------- Borrar un usuario por su Id-------------------------
+    @DeleteMapping(value="/{id}")
+    @ResponseBody
+    public String borrarUsuario(@PathVariable Long id) {
+        this.srvc.eliminarPorId(id);
+        return "Eliminado el usuario " + id;
+    }
+    // ----------------- Borrar un usuario -------------------------
+    @DeleteMapping(value="/borrar")
+    @ResponseBody
+    public String borrarUsu(@RequestBody Usuario usu) {
+        this.srvc.eliminar(usu);
+        return "Eliminado el usuario " + usu.getId();
+    }
+    // ----------------- Borrar usuarios en bloque -------------------------
+    @DeleteMapping(value="/borravarios")
+    @ResponseBody
+    public String borrarUsuarios(@RequestBody Set<Usuario> usus) {
+        this.srvc.eliminarSet(usus);
+        return "Se han eliminado " + usus.size() + " usuarios";
+    }
+    // ----------------- Guardar un usuario -------------------------
+    @PostMapping(value="/guardar")
+    @ResponseBody
+    public Usuario guardaUsuario(@RequestBody Usuario usu) {
+        // los datos llegan en formato JSON
+        this.srvc.guardar(usu);
+        return usu;
+    }
+    // ----------------- Guardar usuarios en bloque -------------------------
+    @PostMapping(value="/guardavarios")
+    @ResponseBody
+    public String guardaUsuarios(@RequestBody Set<Usuario> usus) {
+        // los datos llegan en formato JSON
+        this.srvc.guardarSet(usus);
+        return ("Se han guardado " +  usus.size() + " usuarios.");
     }
 }
